17ae4d5b8f84f878ccf0aae79bb39fc217737c96
GithubVictrix <99933661+GithubVictrix@users.noreply.github.com>
2022-02-18 09:39:38 -0500

    Update linuxtools.sh

--- linuxtools.sh
+++ linuxtools.sh
@@ -47,3 +47,15 @@ wget https://raw.githubusercontent.com/mbouaklaine/MohamedBouaklaine/main/Powers
 sudo chmod 777 Powershell.sh 
 runuser -l azureuser -c "/home/azureuser/Powershell.sh"
 
+#### INSTALL AZ Bicep###
+# Fetch the latest Bicep CLI binary
+curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
+# Mark it as executable
+chmod +x ./bicep
+# Add bicep to your PATH (requires admin)
+sudo mv ./bicep /usr/local/bin/bicep
+# Verify you can now access the 'bicep' command
+bicep --help
+# Done!
+
+
