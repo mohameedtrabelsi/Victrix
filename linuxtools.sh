24075bfb573f765491e327135ba61eb945d9bfbe
GithubVictrix <99933661+GithubVictrix@users.noreply.github.com>
2022-02-18 18:00:58 -0500

    Update linuxtools.sh

--- linuxtools.sh
+++ linuxtools.sh
@@ -53,7 +53,20 @@ sudo add-apt-repository universe
 sudo apt-get install -y powershell
 
 #### INSTALL AZ Module###
+cd /home/azureuser
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo add-apt-repository universe
+sudo apt-get install -y powershell
+pwsh
+pwsh -c "Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force"
+pwsh -c "Install-Module Az -AllowClobber -Force"
+pwsh -c "Update-Module -Name Az -Force"
 
-wget https://raw.githubusercontent.com/GithubVictrix/Victrix/main/Powershell.sh
-sudo chmod 777 Powershell.sh 
-runuser -l azureuser -c "/home/azureuser/Powershell.sh"
+
+
+
+
